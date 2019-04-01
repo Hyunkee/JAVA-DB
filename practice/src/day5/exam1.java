60d3eb94f839b105189bf771f7a8ba25349ab72e
hyunkee <khk2909@daum.net>
2019-04-01 16:13:42 +0900

    5일차 for중첩을 이용한 * 예제

---
+++ practice/src/day5/exam1.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class exam1 {
+	public static void main(String[] args) {
+		int i, j;
+		for(i=1; ; i++){
+			if(i>3){
+				break;
+			}
+			for(j=1; ; j++){	
+				if(j>5){
+					break;
+				}
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
