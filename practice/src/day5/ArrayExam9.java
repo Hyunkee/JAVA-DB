f619f1784b167f30a1936b6010f2e48aadd3281c
hyunkee <khk2909@daum.net>
2019-04-01 16:16:48 +0900

    5일차 Array 예제9

---
+++ practice/src/day5/ArrayExam9.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class ArrayExam9 {
+	public static void main(String[] args){
+		int [] arr1,arr2; // 둘다배열
+		int arr3[],arr4; // arr3 배열, arr4 변수
+		arr1 = new int[3]; // 배열
+		arr2 = new int[3]; // 배열
+		arr3 = new int[3]; // 배열
+		//arr4 = new int[3]; // 변수  arr4 : int형		
+	}
+}
