41641e9b42b9ca53c0aa9902e718bf4ac7fc7ae3
hyunkee <khk2909@daum.net>
2019-04-01 16:16:20 +0900

    5일차 Array 예제4

---
+++ practice/src/day5/ArrayExam4.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class ArrayExam4 {
+	public static void main(String[] args){
+		/* 변수의 유효 범위 
+		 * 일반 변수는 선언되면 해당 변수를 감싸고 있는 괄호안에서 유효 하다
+		 * 
+		 * */
+		for(int i = 0;i<10; i++){ // 반복문안에서만 쓰일 경우 
+			System.out.println(i);
+		}
+		for(int i = 0;i<5; i++){
+			System.out.println(i);
+		}
+	}
+}
