1980e31e1b580fd6d9572802d98eed85f088f68d
hyunkee <khk2909@daum.net>
2019-04-03 12:51:55 +0900

    7일차 메소드예제

---
+++ practice/src/day7/MethodExam.java
@@ -0,0 +1,28 @@
+package day7;
+
+public class MethodExam {
+	public static void main(String[] args){
+		int [] arr = new int[5];
+		printArr(arr);
+		initArr(arr,1);
+		printArr(arr);
+	}
+	/* 기능 : 배열을 num으로 초기화 하는 메소드
+	 * 매개변수 : 배열, num => int[] arr, int num
+	 * 리턴타입 : 없다 => void
+	 * 매소드명 : initArr
+	 * */
+	public static void initArr(int[] arr, int num){
+		//arr = new int[10];
+		for(int i = 0; i<arr.length; i++){
+			arr[i] = num;
+		}
+	}
+	public static void printArr(int[] arr){
+		for(int tmp:arr){
+			System.out.print(tmp + " ");
+		}
+		System.out.println();
+	}
+	
+}
