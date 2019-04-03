f4ec5d4916a818e678c47e2d5159083c32b536de
hyunkee <khk2909@daum.net>
2019-04-03 12:51:11 +0900

    7일차 시험 문제1

---
+++ practice/src/day7/exam1.java
@@ -0,0 +1,12 @@
+package day7;
+
+public class exam1 {
+	public static void main(String[] args){
+		// 구구단 전체를 출력하는 코드를 작성하세요
+		for(int i=2;i<=9;i++ ){
+			for(int j=1 ;j<=9 ;j++ ){
+				System.out.println(i+"x"+j+"="+i*j);
+			}
+		}
+	}
+}
