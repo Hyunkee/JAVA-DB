fa946c0f0e17ac6460decc8f40902eef34c3cfd2
hyunkee <khk2909@daum.net>
2019-04-04 17:01:02 +0900

    8일차 Method 예제4

---
+++ practice/src/day8/MethodExam4.java
@@ -0,0 +1,28 @@
+package day8;
+
+public class MethodExam4 {
+	public static void main(String[] args) {
+		//재귀호출 
+		//java.lang.StackOverflowError 발생
+		//print();
+		System.out.println(factorial(5));
+	}
+	// 잘못된 재귀호출 메소드 예제 
+	public static void print(){
+		System.out.println("Hello");
+		print();
+	}
+	/* 기능: 정수 num이 주어지면 주어진 정수 num!를 알려주는 메소드
+	 * 매개변수 : int num
+	 * 리턴타입 : num! => int
+	 * 메소드명 : factorial
+	 * */
+	public static int factorial(int num){
+		if(num == 1 || num == 0)
+			return 1;
+		if(num < 0)
+			return -1;
+		return factorial(num-1)*num;
+	}
+	
+}
