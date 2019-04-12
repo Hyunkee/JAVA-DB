3d1cd19e03d837fafd4f87cda7196e9ffc430d2a
hyunkee <khk2909@daum.net>
2019-04-12 12:49:20 +0900

    11일차 Circle 예제1

---
+++ practice/src/day11/Circle.java
@@ -0,0 +1,30 @@
+package day11;
+
+public class Circle extends Shape {
+	/*Shape 클래스를 이용하여 Circle을 구현하십시오.*/
+	public double getRadius(){
+		return getWidth()/2;
+	}
+	public double getArea(){
+		return getRadius()*getRadius()*Math.PI;
+	}
+	@Override
+	public void print(){
+		System.out.println("--------------------------------");
+		System.out.print("중심점 : ");
+		getCenter().print();
+		System.out.println("반지름 : " + getRadius());
+		System.out.println("넓이 : " + getArea());
+		System.out.println("--------------------------------");
+	}
+	public void resize(double r){
+		setWidth(r*2);
+		setHeight(r*2);
+	}
+	public Circle(){
+		
+	}
+	public Circle(int x, int y, int r){
+		super(x,y,r*2,r*2);
+	}
+}
