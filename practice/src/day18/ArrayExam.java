cd014d53dabe3caa8c8d19e56da2b810fa14b955
hyunkee <khk2909@daum.net>
2019-04-18 17:16:24 +0900

    18일차 배열 예제

---
+++ practice/src/day18/ArrayExam.java
@@ -0,0 +1,59 @@
+package day18;
+
+import java.util.Scanner;
+
+
+import day18.UniversityStudent;
+import day18.StudentManager;
+
+public class ArrayExam {
+
+	public static void main(String[] args) {
+		//StudentManager의 객체 멤버 메소드 printMenu를 호출
+		//>> StudentManager 객체를 생성해야한다
+		StudentManager mng = new StudentManager();
+		int menu = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		do{
+			mng.printMenu();
+			menu = scan.nextInt();
+			UniversityStudent s;
+			switch(menu){
+			case 1:
+				System.out.println("추가할 학생 정보를 입력하세요.");
+				s = mng.inputSearchStudent(scan);
+				mng.insert(s);
+				break;	
+			case 2:
+				System.out.println("수정할 학생 정보를 입력하세요.");
+				s = mng.inputSearchStudent(scan);
+				mng.modify(s);
+				break;
+			case 3:	
+				System.out.println("삭제할 학생 정보를 입력하세요.");
+				s = mng.inputSearchStudent(scan);
+				mng.delete(s);
+				break;
+			case 4:	
+				System.out.println("----학생정보 출력----");
+				mng.print();			break;
+			case 5:	
+				System.out.println("프로그램을 종료합니다.!!!");
+				break;
+			default:
+				System.out.println("잘못된 메뉴");
+			}
+			
+		}while(menu != 5);
+		/*for( ;menu != 5 ;){
+			mng.printMenu();
+			menu = scan.nextInt();
+		}*/
+		/*while(menu!=5){
+			mng.printMenu();
+			menu = scan.nextInt();
+		}*/
+		scan.close();
+	}
+}
