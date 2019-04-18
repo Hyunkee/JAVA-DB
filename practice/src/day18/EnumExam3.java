b2eaef7924f9f5b3465166519de2e1216d028735
hyunkee <khk2909@daum.net>
2019-04-18 17:16:55 +0900

    18일차 ENUM 예제3

---
+++ practice/src/day18/EnumExam3.java
@@ -0,0 +1,14 @@
+package day18;
+
+public class EnumExam3 {
+
+	public static void main(String[] args) {
+		//UniversityStudent std1 = 
+				//new UniversityStudent(Gender.MALE, Grade.ETC, Major.COMPUTER, "김현기",2019160135);
+		//System.out.println(std1);
+		/*std1.setUniversityName("그린");
+		std1.setAvrage(3.5);
+		System.out.println(std1);*/
+	}
+
+}
