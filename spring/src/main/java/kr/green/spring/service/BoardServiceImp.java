4909fd427214715f089c3e47ee0ca02983994503
hyunkee <khk2909@daum.net>
2019-07-15 13:16:47 +0900

    19. 07. 15  BoardServiceImp 수정

--- spring/src/main/java/kr/green/spring/service/BoardServiceImp.java
+++ spring/src/main/java/kr/green/spring/service/BoardServiceImp.java
@@ -2,11 +2,14 @@ package kr.green.spring.service;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.green.spring.dao.BoardDAO;
 import kr.green.spring.vo.BoardVO;
+import kr.green.spring.vo.MemberVO;
 
 @Service
 public class BoardServiceImp implements BoardService {
@@ -44,12 +47,30 @@ public class BoardServiceImp implements BoardService {
 			return false;		
 		boardDao.updateBoard(tmp);
 		return true;		
+	}	
+
+	@Override
+	public void insert(BoardVO boardVO) {		
+		boardDao.insertBoard(boardVO);		
 	}
 
 	@Override
-	public ArrayList<BoardVO> delete() {		
-		return boardDao.deleteBoard();
+	public void delete(BoardVO boardVO) {
+		
+		boardDao.deleteBoard(boardVO);		
 	}
 
-	
+	@Override
+	public boolean isWriter(Integer num, HttpServletRequest r) {
+		MemberVO user = (MemberVO)r.getSession().getAttribute("user");
+		BoardVO bVo = boardDao.getBoard(num);
+		if(bVo == null || user == null) {
+			return false;
+		}
+		if(bVo.getWriter() != null && bVo.getWriter().equals(user.getId())) {
+			return true;
+		}
+		return false;
+	}
+
 }
