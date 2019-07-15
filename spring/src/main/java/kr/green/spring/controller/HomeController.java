16e8de767f732370ad1abfba8990a4f56abc6935
hyunkee <khk2909@daum.net>
2019-07-15 13:17:42 +0900

    19. 07. 15  HomeController 주석처리 수정

--- spring/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring/src/main/java/kr/green/spring/controller/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 	// 서버 부분을 제외한 URL이 "/" 이고, 방식이 GET이면 home 메소드를 실행
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		logger.info("회원가입페이지 실행"); // 서버기록 관리하는 함수		
+		//logger.info("회원가입페이지 실행"); // 서버기록 관리하는 함수		
 		
 		
 		return "home";
@@ -40,7 +40,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signupGet(Model model) {
-		logger.info("회원가입페이지 실행"); // 서버기록 관리하는 함수		
+		//logger.info("회원가입페이지 실행"); // 서버기록 관리하는 함수		
 		
 		//회원가입 진행되어야 함
 		
@@ -49,7 +49,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signupPost(MemberVO mVo) {
-		logger.info("회원가입 진행중"); // 서버기록 관리하는 함수		
+		//logger.info("회원가입 진행중"); // 서버기록 관리하는 함수		
 		
 		//회원가입 진행되어야 함
 		if(memberService.signup(mVo))
@@ -61,14 +61,14 @@ public class HomeController {
 	}
 	@RequestMapping(value = "/signin", method = RequestMethod.GET)
 	public String signinGet(Model model) {
-		logger.info("로그인페이지 실행"); // 서버기록 관리하는 함수		
+		//logger.info("로그인페이지 실행"); // 서버기록 관리하는 함수		
 		
 		
 		return "signin";
 	}
 	@RequestMapping(value = "/signin", method = RequestMethod.POST)
 	public String signinPost(Model model, MemberVO mVo) {
-		logger.info("로그인 진행중"); // 서버기록 관리하는 함수		
+		//logger.info("로그인 진행중"); // 서버기록 관리하는 함수		
 		System.out.println(mVo);
 		//회원가입 진행되어야 함
 		MemberVO user = memberService.signin(mVo);
@@ -80,21 +80,21 @@ public class HomeController {
 	}
 	@RequestMapping(value = "/member", method = RequestMethod.GET)
 	public String memberGet(Model model) {
-		logger.info("회원정보페이지 실행"); // 서버기록 관리하는 함수		
+		//logger.info("회원정보페이지 실행"); // 서버기록 관리하는 함수		
 		
 		
 		return "member";
 	}
 	@RequestMapping(value = "/member/modify", method = RequestMethod.GET)
 	public String memberModifyGet(Model model) {
-		logger.info("회원정보 수정페이지 실행"); // 서버기록 관리하는 함수		
+		//logger.info("회원정보 수정페이지 실행"); // 서버기록 관리하는 함수		
 		
 		
 		return "member/modify";
 	}
 	@RequestMapping(value = "/member/modify", method = RequestMethod.POST)
 	public String memberModifyPost(Model model, MemberVO mVo, String oldPw) {
-		logger.info("회원정보 수정페이지 진행중"); // 서버기록 관리하는 함수		
+		//logger.info("회원정보 수정페이지 진행중"); // 서버기록 관리하는 함수		
 		System.out.println(mVo);
 		System.out.println(oldPw);
 		
