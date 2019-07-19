87467a8c00240074155069456c2d7558212c50ca
hyunkee <khk2909@daum.net>
2019-07-19 16:58:23 +0900

    19. 07. 19 MemberServiceImp 수정 및 추가

--- spring/src/main/java/kr/green/spring/service/MemberServiceImp.java
+++ spring/src/main/java/kr/green/spring/service/MemberServiceImp.java
@@ -1,6 +1,11 @@
 package kr.green.spring.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.green.spring.dao.MemberDAO;
@@ -11,6 +16,10 @@ public class MemberServiceImp implements MemberService {
 	
 	@Autowired
 	MemberDAO memberDao;
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	@Override
 	public boolean signup(MemberVO mVo) {
@@ -19,10 +28,13 @@ public class MemberServiceImp implements MemberService {
 		// 없으면 회원가입 진행
 		if(mVo == null)
 			return false;
-		mVo.setName("");
-			
+		mVo.setName("");			
 		if(memberDao.getMember(mVo.getId()) != null)
 			return false;
+		// 회원가입창에서 입력받은 암호를 암호화 시킴
+		String encodePw = passwordEncoder.encode(mVo.getPw());
+		// 회원 정보의 비밀번호를 암호화된 비밀번호로 변경
+		mVo.setPw(encodePw);
 		memberDao.signup(mVo);
 		return true;
 	}
@@ -34,7 +46,7 @@ public class MemberServiceImp implements MemberService {
 		MemberVO oVo = memberDao.getMember(mVo.getId()); //dao에게 id와 일치하는 회원 정보를 가져오게 시키고 저장한 후,
 		if(oVo == null)			
 			return null; // 가져오게 시키고 저장한 후, 저장된 회원 정보가 없으면(null) 컨트롤러에게 회원 아니라고 알려주고
-		if(oVo.getPw().equals(mVo.getPw()))		
+		if(passwordEncoder.matches(mVo.getPw(), oVo.getPw())) 		
 			return oVo;//있으면 가져온 회원정보 비밀번호와 입력한
 						//회원정보 비밀번호를 비교하여 같으면 컨트롤러에게 회원이라고 알려주고, 다르면 아니라고 알려준다.		
 		return null;
@@ -49,6 +61,73 @@ public class MemberServiceImp implements MemberService {
 			return true;
 		}
 		return false;
+	}	
+
+	@Override
+	public boolean isMember(String id) {		
+		if(memberDao.getMember(id) == null) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String getVal(String id) {
+		String [] arr = id.split("=");
+		if(arr.length == 2)			
+			return arr[1];
+		else
+			return "";
+	}
+
+	@Override
+	public boolean checkMember(String id, String email) {
+		MemberVO user = memberDao.getMember(id);
+		if(user != null && user.getEmail().equals(email)) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String createPw() {
+		String str = "0123456789abcdefghijklmnoqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()";
+		String pw = "";
+		for(int i=0; i<8; i++) {
+			int r = (int)(Math.random()*73);
+			pw += str.charAt(r);
+		}
+		return pw;
+	}
+
+	@Override
+	public void modify(String id, String email, String newPw) {		
+		MemberVO user = memberDao.getMember(id);
+		if(user == null)	return;
+		if(!user.getEmail().equals(email))	return;
+		String encodePw = passwordEncoder.encode(newPw);
+		user.setPw(encodePw);
+		memberDao.modify(user);
+	}
+
+	@Override
+	public void sendMail(String email, String title, String contents) {
+		String setfrom = "stajun@naver.com";
+	    try {
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper 
+	            = new MimeMessageHelper(message, true, "UTF-8");
+
+	        messageHelper.setFrom(setfrom);  // 보내는사람 생략하거나 하면 정상작동을 안함
+	        messageHelper.setTo(email);     // 받는사람 이메일
+	        messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+	        messageHelper.setText(contents);  // 메일 내용
+
+	        mailSender.send(message);
+	    } catch(Exception e){
+	        System.out.println(e);
+	    }
+		
 	}
 	
 }
