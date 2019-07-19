50f5f224deabd6a76ab082877278e8389d254de4
hyunkee <khk2909@daum.net>
2019-07-19 16:57:43 +0900

    19. 07. 19 HomeController 수정및 추가 ( 아이디체크,비밀번호체크,메일체크,메일보내기,임시비밀번호 보내기)

--- spring/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring/src/main/java/kr/green/spring/controller/HomeController.java
@@ -1,15 +1,24 @@
 package kr.green.spring.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.green.spring.service.MemberService;
 import kr.green.spring.vo.MemberVO;
@@ -25,15 +34,15 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
-	MemberService memberService;	
+	MemberService memberService;
+	
 	
 	
 	
 	// 서버 부분을 제외한 URL이 "/" 이고, 방식이 GET이면 home 메소드를 실행
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		//logger.info("회원가입페이지 실행"); // 서버기록 관리하는 함수		
-		
+		//logger.info("회원가입페이지 실행"); // 서버기록 관리하는 함수	
 		
 		return "home";
 	}
@@ -110,4 +119,76 @@ public class HomeController {
 		session.removeAttribute("user");
 		return "redirect:/";
 	}
+	@RequestMapping(value ="/dup")
+	@ResponseBody
+	public Map<Object, Object> idcheck(@RequestBody String id){
+
+	    Map<Object, Object> map = new HashMap<Object, Object>();
+	    // 변수 id에 저장된 아이디가 회원 아이디인지 아닌지 확인하여 isMember변수에 담아 보낸다.
+	    boolean isMember = memberService.isMember(id);
+	    map.put("isMember",isMember);	    
+	    System.out.println(map);
+	    return map;
+	}
+	@RequestMapping(value = "/mail/mailForm")
+	public String mailForm() {
+
+	    return "mail";
+	}  
+
+	// mailSending 코드
+	@RequestMapping(value = "/mail/mailSending")
+	public String mailSending(HttpServletRequest request) {
+
+	    String setfrom = "stajun@naver.com";         
+	    String tomail  = request.getParameter("tomail");     // 받는 사람 이메일
+	    String title   = request.getParameter("title");      // 제목
+	    String content   = request.getParameter("content");
+	    memberService.sendMail(tomail, title, content);
+	    
+
+	    return "redirect:/mail/mailForm";
+	}
+	
+	@RequestMapping(value = "password/find", method = RequestMethod.GET)
+	public String passwordFindGet() {		
+		return "member/find";
+	}
+	
+	// 아이디와 이메일체크 
+	@RequestMapping(value ="/checkemail")
+	@ResponseBody
+	public Map<Object, Object> emailcheck(@RequestBody String str){
+
+	    Map<Object, Object> map = new HashMap<Object, Object>();
+	    // 변수 id에 저장된 아이디가 회원 아이디인지 아닌지 확인하여 isMember변수에 담아 보낸다.
+	    
+	    String [] arr = str.split("&");
+	    String id = arr[0];
+	    String email = "";	    
+	    try {
+			email = URLDecoder.decode(arr[1], "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}	    	    
+	    id = memberService.getVal(id);
+	    email = memberService.getVal(email);
+	    
+	    boolean isOk = memberService.checkMember(id,email);
+	    map.put("isOk", isOk);
+	    return map;
+	}
+	// 임시 비밀번호 메일로 발송
+	@RequestMapping(value = "/password/send")
+	public String passwordSend(String email,String id) {
+		//비밀번호 생성
+		String newPw = memberService.createPw();
+		//생성된 비밀번호 DB에 저장
+		memberService.modify(id, email, newPw);
+		//이메일 발송
+		String title = "변경된 비밀번호입니다.";
+		String contents = "변경된 비밀번호입니다.\n"+newPw+"\n";
+		memberService.sendMail(email,title,contents);
+	    return "send";
+	}  
 }
