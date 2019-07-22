32ebf76481b86cf87841de64bf70e526e1553c13
hyunkee <khk2909@daum.net>
2019-07-22 16:57:55 +0900

    19. 07. 22 CharSequence 설명추가

--- spring/src/main/java/kr/green/spring/service/MemberServiceImp.java
+++ spring/src/main/java/kr/green/spring/service/MemberServiceImp.java
@@ -46,7 +46,8 @@ public class MemberServiceImp implements MemberService {
 		MemberVO oVo = memberDao.getMember(mVo.getId()); //dao에게 id와 일치하는 회원 정보를 가져오게 시키고 저장한 후,
 		if(oVo == null)			
 			return null; // 가져오게 시키고 저장한 후, 저장된 회원 정보가 없으면(null) 컨트롤러에게 회원 아니라고 알려주고
-		if(passwordEncoder.matches(mVo.getPw(), oVo.getPw())) 		
+		if(passwordEncoder.matches(mVo.getPw(), oVo.getPw())) 
+			//CharSequence: String과 반대로 문자열을 변형, 가공을 할 수 있기때문에 쓰임 		
 			return oVo;//있으면 가져온 회원정보 비밀번호와 입력한
 						//회원정보 비밀번호를 비교하여 같으면 컨트롤러에게 회원이라고 알려주고, 다르면 아니라고 알려준다.		
 		return null;
